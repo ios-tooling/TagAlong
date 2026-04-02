f7ddf6ae68e18703c209923210e8a713beb280f3
Ben Gottlieb <ben@standalone.com>
2026-04-02 08:00:17 -0500

    updated TagView

--- Sources/TagAlong/TagsView.swift
+++ Sources/TagAlong/TagsView.swift
@@ -11,28 +11,45 @@ import SwiftUI
 @available(iOS 17, macOS 14, *)
 public struct TagsView: View {
 	let tags: [Tag]
-	
-    public init(tags: TagCollection) {
+	let showRemove: Bool
+	let onTap: ((Tag) -> Void)?
+
+    public init(tags: TagCollection, showRemove: Bool = false, onTap: ((Tag) -> Void)? = nil) {
         self.tags = tags.tags
+        self.showRemove = showRemove
+        self.onTap = onTap
     }
-    
-    public init(_ tags: TagCollection) {
+
+    public init(_ tags: TagCollection, showRemove: Bool = false, onTap: ((Tag) -> Void)? = nil) {
         self.tags = tags.tags
+        self.showRemove = showRemove
+        self.onTap = onTap
     }
-    
+
 	public var body: some View {
 		if !tags.isEmpty {
 			HStack(spacing: 4) {
 				ForEach(tags, id: \.self) { tag in
-					Text(tag.name)
-						.font(.caption2)
-						.padding(.horizontal, 6)
-						.padding(.vertical, 2)
-						.background(tag.tagColor)
-						.foregroundStyle(tag.tagColor.textColor)
-						.clipShape(Capsule())
+					tagCapsule(tag)
 				}
 			}
 		}
+	}
+
+	private func tagCapsule(_ tag: Tag) -> some View {
+		HStack(spacing: 3) {
+			Text(tag.name)
+			if showRemove {
+				Image(systemName: "xmark")
+					.font(.system(size: 8, weight: .bold))
+			}
+		}
+		.font(.caption2)
+		.padding(.horizontal, 6)
+		.padding(.vertical, 2)
+		.background(tag.tagColor)
+		.foregroundStyle(tag.tagColor.textColor)
+		.clipShape(Capsule())
+		.onTapGesture { onTap?(tag) }
 	}
 }
